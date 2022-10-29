65e1a7327ef780516cbcaac59c7de236485f9d98
vandit vasa <vasavandit@gmail.com>
2022-10-29 15:29:10 +0530

    added Constants.java

--- app/src/main/java/cf/vandit/movie_app/utils/Constants.java
+++ app/src/main/java/cf/vandit/movie_app/utils/Constants.java
@@ -1,11 +1,11 @@
 package cf.vandit.movie_app.utils;
 
 public class Constants {
-    public static final String API_KEY = "TMDB-API-KEY";
-    public static final String YOUTUBE_API_KEY = "YOUTUBE-API-KEY";
+    public static final String API_KEY = "e904cd25f93143f938021b6f300ca5f6";
+    public static final String YOUTUBE_API_KEY = "AIzaSyDvIMRSVE0nl4pCDCj-JdBkuY8kG86AEqA";
     public static final String BASE_URL = "https://api.themoviedb.org/3/";
-    public static final String MOVIE_STREAM_URL = "CANNOT-BE-DISCLOSED";
-    public static final String SERIES_STREAM_URL = "CANNOT-BE-DISCLOSED";
+    public static final String MOVIE_STREAM_URL = "https://gomo.to/movie/";
+    public static final String SERIES_STREAM_URL = "https://gomo.to/show/";
     public static final String IMAGE_LOADING_BASE_URL_1280 = "https://image.tmdb.org/t/p/w1280/";
     public static final String IMAGE_LOADING_BASE_URL_342 = "https://image.tmdb.org/t/p/w342/";
     public static final String IMAGE_LOADING_BASE_URL_780 = "https://image.tmdb.org/t/p/w780/";
@@ -17,13 +17,32 @@ public class Constants {
     public static final String PERSON_ID = "person_id";
 
     public static final String VIEW_ALL_MOVIES_TYPE = "type_view_all_movies";
-    public static final int POPULAR_MOVIES_TYPE = 2;
-    public static final int TOP_RATED_MOVIES_TYPE = 3;
+    public static final int POPULAR_MOVIES_TYPE = 1;
+    public static final int TOP_RATED_MOVIES_TYPE = 2;
+    public static final int ACTION_MOVIES_TYPE = 28;
+    public static final int ADVENTURE_MOVIES_TYPE = 12;
+    public static final int ANIMATION_MOVIES_TYPE = 16;
+    public static final int COMEDY_MOVIES_TYPE = 35;
+    public static final int CRIME_MOVIES_TYPE = 80;
+    public static final int DOCUMENTARY_MOVIES_TYPE = 99;
+    public static final int DRAMA_MOVIES_TYPE = 18;
+    public static final int FAMILY_MOVIES_TYPE = 10751;
+    public static final int FANTASY_MOVIES_TYPE = 14;
+    public static final int HISTORY_MOVIES_TYPE = 36;
+    public static final int HORROR_MOVIES_TYPE = 27;
+    public static final int MUSIC_MOVIES_TYPE = 10402;
+    public static final int MYSTERY_MOVIES_TYPE = 9648;
+    public static final int ROMANCE_MOVIES_TYPE = 10749;
+    public static final int SCIFI_MOVIES_TYPE = 878;
+    public static final int TV_MOVIES_TYPE = 10770;
+    public static final int THRILLER_MOVIES_TYPE = 53;
+    public static final int WAR_MOVIES_TYPE = 10752;
+    public static final int WESTERN_MOVIES_TYPE = 37;
 
     public static final String VIEW_ALL_TV_SHOWS_TYPE = "type_view_all_tv_shows";
-    public static final int ON_THE_AIR_TV_SHOWS_TYPE = 1;
-    public static final int POPULAR_TV_SHOWS_TYPE = 2;
-    public static final int TOP_RATED_TV_SHOWS_TYPE = 3;
+    public static final int ON_THE_AIR_TV_SHOWS_TYPE = 101;
+    public static final int POPULAR_TV_SHOWS_TYPE = 102;
+    public static final int TOP_RATED_TV_SHOWS_TYPE = 103;
 
     public static final int TAG_FAV = 0;
     public static final int TAG_NOT_FAV = 1;
